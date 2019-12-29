0cfec29fa9c114ea55eff0f6241b6987bc6643e8
Parvez Khan Pathan <41503632+iamParvezKhan25@users.noreply.github.com>
2019-12-29 15:00:23 +0530

    Add files via upload

---
+++ crud/books/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'books'
+
+urlpatterns = [
+    path('', views.BookList.as_view(), name='book_list'),
+    path('view/<int:pk>', views.BookView.as_view(), name='book_view'),
+    path('new/', views.BookCreate.as_view(), name='book_new'),
+    path('view/<int:pk>', views.BookView.as_view(), name='book_view'),
+    path('edit/<int:pk>', views.BookUpdate.as_view(), name='book_edit'),
+    path('delete/<int:pk>', views.BookDelete.as_view(), name='book_delete')
+]
